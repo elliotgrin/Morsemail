fefd53b5c4566f6b406d75eab2fd6f259db7c837
rkuznetsov1 <roma.sk8er@gmail.com>
2016-12-26 19:31:59 +0300

    initial commit

---
+++ app/src/main/java/com/morsemail/android/morsemail/MainActivity.java
@@ -0,0 +1,174 @@
+package com.morsemail.android.morsemail;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Vibrator;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Launcher activity listening the volume keys events
+ * and decoding Morse code, which entered via volume keys
+ */
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * A char encoded Morse code
+     */
+    private ArrayList<Integer> charCode;
+
+    /**
+     * Morse code
+     */
+    private MorseCode morseCode;
+
+    /**
+     * Mail input field
+     */
+    private EditText mOutEditText;
+
+    /**
+     * Mail send button
+     */
+    private Button mSendButton;
+
+    /**
+     * Flags of pressing volume buttons
+     */
+    private boolean isShortPress = false, isLongPress = false;
+
+    /**
+     * Array adapter for the message view
+     */
+    private ArrayAdapter<String> mMailArrayAdapter;
+
+    /**
+     * Mail history list view
+     */
+    private ListView mMailListView;
+
+    Vibrator v;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Don't show the keyboard at every startup
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+
+        morseCode = new MorseCode();
+        charCode = new ArrayList<>();
+        mOutEditText = (EditText) findViewById(R.id.res_text_out);
+        mSendButton = (Button) findViewById(R.id.button_send);
+        mMailListView = (ListView) findViewById(R.id.mails);
+
+        v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+        mMailArrayAdapter = new ArrayAdapter<>(this, R.layout.message);
+
+        mMailListView.setAdapter(mMailArrayAdapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mSendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView textView = (TextView) findViewById(R.id.res_text_out);
+                String message = textView.getText().toString();
+                if (message.length() > 0) {
+                    sendEmail(message);
+                    mMailArrayAdapter.add("Mail: " + message);
+                    mOutEditText.setText("");
+                }
+            }
+        });
+    }
+
+    private void sendEmail(String message){
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Hey, I wrote it in Morse code!");
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+
+        startActivity(Intent.createChooser(intent, "Send Email"));
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN || keyCode == KeyEvent.KEYCODE_VOLUME_UP) {
+            event.startTracking();
+            if (isLongPress) {
+                isShortPress = false;
+            } else {
+                isShortPress = true;
+                isLongPress = false;
+            }
+
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) {
+            event.startTracking();
+            if (isShortPress) {
+                v.vibrate(100);
+                charCode.add(0);
+            }
+            isShortPress = true;
+            isLongPress = false;
+            return true;
+        } else if (keyCode == KeyEvent.KEYCODE_VOLUME_UP) {
+            event.startTracking();
+            if (isShortPress) {
+                v.vibrate(100);
+                String code = morseCode.getChar(charCode);
+                mOutEditText.append(code);
+                charCode.clear();
+            }
+            isShortPress = true;
+            isLongPress = false;
+            return true;
+        }
+
+        return super.onKeyUp(keyCode, event);
+    }
+
+    @Override
+    public boolean onKeyLongPress(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) {
+            v.vibrate(200);
+            charCode.add(1);
+            isShortPress = false;
+            isLongPress = true;
+            return true;
+        } else if (keyCode == KeyEvent.KEYCODE_VOLUME_UP) {
+            v.vibrate(200);
+            mOutEditText.append(" ");
+            charCode.clear();
+            isShortPress = false;
+            isLongPress = true;
+            return true;
+        }
+
+        return false;
+    }
+
+}
---
+++ app/src/main/java/com/morsemail/android/morsemail/MorseCode.java
@@ -0,0 +1,82 @@
+package com.morsemail.android.morsemail;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Describes behavior of morse decoding
+ */
+
+public class MorseCode {
+    /**
+     * Mapping a char from Morse code to text
+     */
+    Map<ArrayList<Integer>, String> mCode;
+
+    public MorseCode() {
+        mCode = new HashMap<>();
+
+        // Letters
+        mCode.put(new ArrayList<>(Arrays.asList(0, 1)), "а");
+        mCode.put(new ArrayList<>(Arrays.asList(1, 0, 0, 0)), "б");
+        mCode.put(new ArrayList<>(Arrays.asList(0, 1, 1)), "в");
+        mCode.put(new ArrayList<>(Arrays.asList(1, 1, 0)), "г");
+        mCode.put(new ArrayList<>(Arrays.asList(1, 0, 0)), "д");
+        mCode.put(new ArrayList<>(Arrays.asList(0)), "е");
+        mCode.put(new ArrayList<>(Arrays.asList(0, 0, 0, 1)), "ж");
+        mCode.put(new ArrayList<>(Arrays.asList(1, 1, 0, 0)), "з");
+        mCode.put(new ArrayList<>(Arrays.asList(0, 0)), "и");
+        mCode.put(new ArrayList<>(Arrays.asList(0, 1, 1, 1)), "й");
+        mCode.put(new ArrayList<>(Arrays.asList(1, 0, 1)), "к");
+        mCode.put(new ArrayList<>(Arrays.asList(0, 1, 0, 0)), "л");
+        mCode.put(new ArrayList<>(Arrays.asList(1, 1)), "м");
+        mCode.put(new ArrayList<>(Arrays.asList(1, 0)), "н");
+        mCode.put(new ArrayList<>(Arrays.asList(1, 1, 1)), "о");
+        mCode.put(new ArrayList<>(Arrays.asList(0, 1, 1, 0)), "п");
+        mCode.put(new ArrayList<>(Arrays.asList(0, 1, 0)), "р");
+        mCode.put(new ArrayList<>(Arrays.asList(0, 0, 0)), "с");
+        mCode.put(new ArrayList<>(Arrays.asList(1)), "т");
+        mCode.put(new ArrayList<>(Arrays.asList(0, 0, 1)), "у");
+        mCode.put(new ArrayList<>(Arrays.asList(0, 0, 1, 0)), "ф");
+        mCode.put(new ArrayList<>(Arrays.asList(0, 0, 0, 0)), "х");
+        mCode.put(new ArrayList<>(Arrays.asList(1, 0, 1, 0)), "ц");
+        mCode.put(new ArrayList<>(Arrays.asList(1, 1, 1, 0)), "ч");
+        mCode.put(new ArrayList<>(Arrays.asList(1, 1, 1, 1)), "ш");
+        mCode.put(new ArrayList<>(Arrays.asList(1, 1, 0, 1)), "щ");
+        mCode.put(new ArrayList<>(Arrays.asList(1, 0, 0, 1)), "ь");
+        mCode.put(new ArrayList<>(Arrays.asList(1, 0, 1, 1)), "ы");
+        mCode.put(new ArrayList<>(Arrays.asList(0, 0, 1, 0, 0)), "э");
+        mCode.put(new ArrayList<>(Arrays.asList(0, 0, 1, 1)), "ю");
+        mCode.put(new ArrayList<>(Arrays.asList(0, 1, 0, 1)), "я");
+
+        // Digits
+        mCode.put(new ArrayList<>(Arrays.asList(0, 1, 1, 1, 1)), "1");
+        mCode.put(new ArrayList<>(Arrays.asList(0, 0, 1, 1, 1)), "2");
+        mCode.put(new ArrayList<>(Arrays.asList(0, 0, 0, 1, 1)), "3");
+        mCode.put(new ArrayList<>(Arrays.asList(0, 0, 0, 0, 1)), "4");
+        mCode.put(new ArrayList<>(Arrays.asList(0, 0, 0, 0, 0)), "5");
+        mCode.put(new ArrayList<>(Arrays.asList(1, 0, 0, 0, 0)), "6");
+        mCode.put(new ArrayList<>(Arrays.asList(1, 1, 0, 0, 0)), "7");
+        mCode.put(new ArrayList<>(Arrays.asList(1, 1, 1, 0, 0)), "8");
+        mCode.put(new ArrayList<>(Arrays.asList(1, 1, 1, 1, 0)), "9");
+        mCode.put(new ArrayList<>(Arrays.asList(1, 1, 1, 1, 1)), "0");
+
+        // Punctuation marks
+        mCode.put(new ArrayList<>(Arrays.asList(0, 0, 0, 0, 0, 0)), ".");
+        mCode.put(new ArrayList<>(Arrays.asList(0, 1, 0, 1, 0, 1)), ",");
+        mCode.put(new ArrayList<>(Arrays.asList(1, 0, 1, 0, 1, 0)), ";");
+        mCode.put(new ArrayList<>(Arrays.asList(1, 1, 1, 0, 0, 0)), ":");
+        mCode.put(new ArrayList<>(Arrays.asList(0, 0, 1, 1, 0, 0)), "?");
+        mCode.put(new ArrayList<>(Arrays.asList(1, 1, 0, 0, 1, 1)), "!");
+        mCode.put(new ArrayList<>(Arrays.asList(1, 0, 0, 0, 0, 1)), "-");
+        mCode.put(new ArrayList<>(Arrays.asList(0, 1, 0, 0, 1, 0)), "\"");
+        mCode.put(new ArrayList<>(Arrays.asList(1, 0, 1, 1, 0, 1)), "|"); // opened or closed bracket
+        mCode.put(new ArrayList<>(Arrays.asList(1, 0, 0, 1, 0)), "/");
+    }
+
+    String getChar(ArrayList<Integer> charCode) {
+        return (mCode.get(charCode) != null) ? mCode.get(charCode) : "";
+    }
+}
